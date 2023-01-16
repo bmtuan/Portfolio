79f9e1a171487194bfa1099598cf4abd75aa7677
bmtuan <tuanbm@topcv.vn>
2023-01-16 11:35:41 +0700

    feat: add information

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,29 +1,27 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "imagebaseurl":"https://bmtuan.github.io/",
+    "name": "Bui Manh Tuan",
+    "role": "AI Engineer",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "url":"https://www.linkedin.com/in/bui-manh-tuan-58751224a/",
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"http://github.com/rbhatia46",
+          "url":"https://github.com/bmtuan",
           "className":"fa fa-github"
         },
         {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
+          "name":"facebook",
+          "url":"https://www.facebook.com/bmtuan050720/",
+          "className":"fa fa-facebook"
+        },
       ],
     "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
+    "address":"Hanoi, VietNam",
+    "email":"tuanbm183653@gmail.com",
     "website":"https://rbhatia46.github.io",
     "education":[
       {
