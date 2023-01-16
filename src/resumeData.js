9e5eaea15c0abe40c9f4307503a755759924336b
bmtuan <tuanbm@topcv.vn>
2023-01-16 17:35:46 +0700

    init commit

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,104 +1,126 @@
 let resumeData = {
-    "imagebaseurl":"https://bmtuan.github.io/",
-    "name": "Bui Manh Tuan",
-    "role": "AI Engineer",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/bui-manh-tuan-58751224a/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"https://github.com/bmtuan",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"facebook",
-          "url":"https://www.facebook.com/bmtuan050720/",
-          "className":"fa fa-facebook"
-        },
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"Hanoi, VietNam",
-    "email":"tuanbm183653@gmail.com",
-    "website":"https://rbhatia46.github.io",
-    "education":[
-      {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+  imagebaseurl: "https://bmtuan.github.io/",
+  contact: "https://www.facebook.com/bmtuan050720/",
+  name: "Bui Manh Tuan",
+  role: "AI Engineer",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/bui-manh-tuan-58751224a/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/bmtuan",
+      className: "fa fa-github",
+    },
+    {
+      name: "facebook",
+      url: "https://www.facebook.com/bmtuan050720/",
+      className: "fa fa-facebook",
+    },
+  ],
+  aboutme:
+    "I graduated with a good degree in computer science from Hanoi University of Science and Technology. Tech from here. I am an AI Engineer, proficient in Python programming language. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  address: "Hanoi, VietNam",
+  email: "tuanbm183653@gmail.com",
+  education: [
+    {
+      UniversityName: "Hanoi University of Science and Technology",
+      specialization: "Computer Science",
+      MonthOfPassing: "Aug",
+      YearOfPassing: "2022",
+      Achievements: "Good bachelor's degree",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "AIMENEXT Join Stock Company",
+      specialization: "AI Engineer",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2021",
+      Achievements: "Excellent Developer",
+    },
+    {
+      CompanyName: "TOPCV Viet Nam",
+      specialization: "AI Engineer",
+      MonthOfLeaving: "Aug",
+      YearOfLeaving: "2022",
+      Achievements: "Excellent Developer",
+    },
+  ],
+  skillsDescription: "",
+  skills: [
+    {
+      skillgroup: "Programming Languages",
+      skillname: "Python, Java, C, C++, PHP"
+    },
+    {
+      skillgroup: "Web Dev",
+      skillname: "Html, CSS, JavaScript, React",
+    },
+    {
+      skillgroup: "Database Management",
+      skillname: "MySQL, NoSQL",
+    },
+    {
+      skillgroup: "Deep Learning Frameworks (AI)",
+      skillname: "Pytorch, Tensorflow",
+    },
+    {
+      skillgroup: "Libraries",
+      skillname: "Scikit-learn, Numpy, Pandas, Seaborn, Matplotlib, Mlflow,...",
+    },
+    {
+      skillgroup: "Operating Systems",
+      skillname: "Linux, Windows",
+    },
+    {
+      skillgroup: "Editor",
+      skillname: "MS Office, Latex",
+    },
+    {
+      skillgroup: "Services",
+      skillname: "Docker, GCS, ...",
+    },
+    {
+      skillgroup: "Big Data",
+      skillname: "Hadoop, Spark, Elasticsearch, Kafka, K8s, K9s",
+    },
+  ],
+  portfolio: [
+    {
+      name: "Lisence Plate Recognition",
+      description:
+        "Use YOLO-v3 to detection lisence plate and Convoluiton Neural Network to recoginition character",
+      imgurl: "images/portfolio/project1.jpg",
+      link: "",
+    },
+    {
+      name: "Sentiment Analysis",
+      description: "Emotional classification of comments on e-commerce platform Lazada",
+      imgurl: "images/portfolio/project2.jpg",
+      link: "",
+    },
+    {
+      name: "Pupil Segmentation",
+      description: "Segment pupil from a video",
+      imgurl: "images/portfolio/project3.jpg",
+      link: "",
+    },
+    {
+      name: "Air Quality Prediction",
+      description: "Forecast PM2.5 index from some previous index",
+      imgurl: "images/portfolio/project4.png",
+      link: "",
+    },
+  ],
+  testimonials: [
+    {
+      description: "Education is not the learing of facts, but the training of the mind think.",
+      name: "Albert Einstein",
+    },
+  ],
+};
+
+export default resumeData;
